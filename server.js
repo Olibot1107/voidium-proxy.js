a7314dff5725660e31e7e44709ba6ff3d7aa280e
Olibot1107 <ojdawson1107@gmail.com>
2026-03-14 00:05:33 +0000

    changed port from 3000 to 1234 due to port 3000 being used on the pannel node

--- server.js
+++ server.js
@@ -86,6 +86,6 @@ async function fetchweb(url, req) {
     }
 }
 
-app.listen(3000, () => {
-    console.log('Example app listening on port 3000!');
+app.listen(1234, () => {
+    console.log('Example app listening on port 1234!');
 });
